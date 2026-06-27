b820106d1c1b55a9ddef165fe237905037d3da8a
Jhon Brix Brion <95483281+Brixie71@users.noreply.github.com>
2026-06-27 21:45:07 +0800

    Updated Mod Version

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,4 +1,8 @@
+// FIXICS script_version.hpp
+// Updated: 2026-06-27
+// Commit:  9f616e5 → next release
+
 #define MAJOR 1
-#define MINOR 0
+#define MINOR 1
 #define PATCH 0
-#define BUILD 1
+#define BUILD 0
